c558695395db8e30d7f2a30820ddc53d0eab2361
JT Dewey <dewey.jt@gmail.com>
2023-12-14 14:37:24 -0700

    initial bingo game commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
+  useLoaderData,
   Links,
   LiveReload,
   Meta,
@@ -8,18 +7,48 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import styles from "./tailwind.css";
+import clsx from "clsx";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
+import { themeSessionResolver } from "./sessions.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+}
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+  return (
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,97 @@
+import { useState } from "react";
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+import { ModeToggle } from "~/components/mode-toggle";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Bingo" },
+    { name: "description", content: "Let's play bingo!" },
   ];
 };
 
+type Result = { column: string; value: number };
+
 export default function Index() {
+  const [numbers, setNumbers] = useState<number[]>([]);
+  const [result, setResult] = useState<Result>({ column: "", value: NaN });
+  const [results, setResults] = useState<Result[]>([]);
+
+  function initialize() {
+    setResult({ column: "", value: NaN });
+    setResults([]);
+
+    const nums: number[] = [];
+    let i = 0;
+
+    while (i < 75) {
+      nums.push(i + 1);
+      i++;
+    }
+
+    return nums;
+  }
+
+  function addResult(pick: number) {
+    let letter = "";
+
+    switch (true) {
+      case pick < 16:
+        letter = "B";
+        break;
+      case pick < 31:
+        letter = "I";
+        break;
+      case pick < 46:
+        letter = "N";
+        break;
+      case pick < 61:
+        letter = "G";
+        break;
+      case pick < 76:
+        letter = "O";
+        break;
+    }
+
+    const result = { column: letter, value: pick };
+    setResult(result);
+    setResults([...results, result]);
+  }
+
+  function pickANumber() {
+    const numBank = !numbers.length ? initialize() : numbers;
+    const pick: number = numBank.splice(Math.random() * numBank.length, 1)[0];
+
+    addResult(pick);
+    setNumbers(numBank);
+  }
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col container justify-between h-screen p-2">
+      <div className="flex flex-row justify-between">
+        <h1 className="text-3xl">Bingo</h1>
+        <ModeToggle />
+      </div>
+      <div className="container flex justify-center h-full items-center">
+        <div className="flex rounded-full border-8 w-80 h-80 items-center justify-center font-mono text-9xl">
+          {result.column && (
+            <h1>
+              <span>{result.column}</span>
+              <span>{result.value}</span>
+            </h1>
+          )}
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-between">
+        <Button onClick={pickANumber}>Pick a number</Button>
+        <Button onClick={initialize}>Reset</Button>
+      </div>
+
+      {false &&
+        results.map((result) => (
+          <h3 key={result.value}>{result.column + result.value}</h3>
+        ))}
     </div>
   );
 }
